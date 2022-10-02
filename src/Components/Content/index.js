91ece9709c21807c73afe09e0000512f85d341e5
Lucasm4sco <lmbp202@gmail.com>
2022-10-01 23:40:27 -0300

    layout criado

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -5,6 +5,15 @@ const Content = ({darkMode}) => (
     <section className={darkMode ? 'dark-mode' : 'light-mode'}>
         <div className="container">
             <h1>Bem-vindo a Barber Shop</h1>
+            <p>
+                <strong>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.</strong>
+            </p>
+            <p className="descricao">
+                Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
+            </p>
+            <p className="autor">
+                S. Kelly
+            </p>
         </div>
     </section>
 );
